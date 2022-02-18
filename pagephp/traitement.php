b3cc56e0206b857f46cb914ac59224f8b48853fd
Nowrei <furlancedric@gmail.com>
2022-02-18 08:54:15 +0100

    e

--- pagephp/traitement.php
+++ pagephp/traitement.php
@@ -10,7 +10,10 @@ $aujourdhui = date("Y-m-d");
 $diff = date_diff(date_create($_POST['age']), date_create($aujourdhui));
   echo 'Vous avez '.$diff->format('%y').' ans';
 
-
+  if($diff->format('%y')< 18 ) {
+    header('location: https://www.gulli.fr/%27');
+}else {
+    header('location: https://www.amazon.com/%27');
 
 
 
